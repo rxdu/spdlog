2cb86f63b1c0f4696bcf0f0bb241459f1485076e
Ruixiang Du <ruixiang@nutonomy.com>
2017-07-28 11:12:31 -0400

    signal handling works

--- include/spdlog/details/logger_impl.h
+++ include/spdlog/details/logger_impl.h
@@ -25,7 +25,7 @@ inline spdlog::logger::logger(const std::string& logger_name, const It& begin, c
     _last_err_time(0),
     _msg_counter(1)  // message counter will start from 1. 0-message id will be reserved for controll messages
 {
-    spdlog::details::installCrashHandler();
+    spdlog::installCrashHandler();
 
     _err_handler = [this](const std::string &msg)
     {
--- signal/test_signal.cpp
+++ signal/test_signal.cpp
@@ -8,19 +8,23 @@ int main()
 {
     std::cout << "testing signal" << std::endl;
 
-    // size_t q_size = 4096; //queue size must be power of 2
-    // spdlog::set_async_mode(q_size);
-    // auto async_file = spdlog::daily_logger_st("async_file_logger", "./async_log.log");
+    spdlog::set_async_mode(256);
+    auto file_logger = spdlog::rotating_logger_mt("file_logger", "./test_log.log", 1048576 * 5, 1);
 
-    spdlog::set_async_mode(8192);
-    auto file_logger = spdlog::rotating_logger_mt("file_logger", "./test_log", 1048576 * 5, 1);
-
+    std::cout << "started loop..." << std::endl;
     int i = 0;
     while(true)
     {
         file_logger->info("Async message #{}, #{}, #{}, #{}, #{}", i, i+1, i+2, i+3, i+4);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         i++;
+
+        // if(i == 50)
+        // {
+        //     std::cout << "raising signal" << std::endl;
+        //     //std::raise(SIGABRT);
+        //     std::raise(SIGINT);
+        // }
     }
 
 }
